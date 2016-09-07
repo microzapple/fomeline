31a8563fdb6edacfd06cad4d20270ce0dd596a9c
Oziel Guimaraes <microzapple@gmail.com>
2016-09-07 08:58:33 -0400

    Add tabbed page

--- FomeLine/FomeLine/Views/HomeView.xaml.cs
+++ FomeLine/FomeLine/Views/HomeView.xaml.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Xamarin.Forms;
 
 namespace FomeLine.Views
 {
-    public partial class HomeView : ContentPage
+    public partial class HomeView : TabbedPage
     {
         public HomeView()
         {
